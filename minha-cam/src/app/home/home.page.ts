f7b87b7d0a49cb531f9e7ffcc1ee6c4659107175
Isabela <isa.gh.monteiro@gmail.com>
2020-10-29 22:19:37 -0300

    'funfando'

--- minha-cam/src/app/home/home.page.ts
+++ minha-cam/src/app/home/home.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { Plugins, CameraResultType, CameraSource} from '@capacitor/core';
+import { Plugins, CameraResultType, CameraSource, CameraPhoto} from '@capacitor/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient} from '@angular/common/http';
+ 
 
 
 @Component({
@@ -14,7 +14,9 @@ export class HomePage {
 
   photo: SafeResourceUrl;
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient ) {}
+  
+  //private async savePicture(cameraPhoto: CameraPhoto) { }
 
   async tirarfoto() {
     const image = await Plugins.Camera.getPhoto({
@@ -23,10 +25,48 @@ export class HomePage {
       resultType: CameraResultType.DataUrl,
       source: CameraSource.Camera
     });
-    
+    //console.log(image);
     this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
-   /* var formData = new FormData();
-    formData.append("image", this.file);
-    this.httpClient.post<any>('http://127.0.0.1:5000/prediction', formData); */
+    //console.log(image.dataUrl);
+    
+    function convertDataUrlToBlob(dataUrl): Blob {
+      const arr = dataUrl.split(',');
+      const mime = arr[0].match(/:(.*?);/)[1];
+      const bstr = atob(arr[1]);
+      let n = bstr.length;
+      const u8arr = new Uint8Array(n);
+  
+      while (n--) {
+          u8arr[n] = bstr.charCodeAt(n);
+      }
+  
+      return new Blob([u8arr], {type: mime});
   }
+  const isa = convertDataUrlToBlob(image.dataUrl);
+  //https://stackoverflow.com/questions/51843950/how-to-convert-base64-dataurl-into-image-in-typescript
+  
+  //const file = new File([convertDataUrlToBlob(image.dataUrl)], 'imagem', {type: `image/png}`});
+    //var imagem = this.sanitizer.sanitize(SecurityContext.RESOURCE_URL, this.sanitizer.bypassSecurityTrustResourceUrl(this.photo));
+    //const savedImageFile = await this.savePicture(image);
+    //var imagem = new Blob( this.photo,"image/jpg")
+
+    var formData = new FormData();
+    formData.append("image", isa);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Basic aXNhYmVsYW1vbnRlaXJvOmNhbWFyaXNhMDE=");
+    //var temp = this.http.post('http://127.0.0.1:5000/prediction', this.photo);
+    //const response = await 
+    fetch('http://127.0.0.1:5000/prediction', {
+      method: 'POST',
+      body: formData,
+      headers: myHeaders,
+      redirect: 'follow'})
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+    //console.log(response);
+    }
+  
 }
--- minha-cam/src/main.ts
+++ minha-cam/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import {defineCustomElements } from '@ionic/pwa-elements/loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 defineCustomElements(window);
 if (environment.production) {
